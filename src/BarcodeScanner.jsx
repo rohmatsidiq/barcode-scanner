922a3aa67b09435efedb4493262671923e19c537
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2024-04-09 06:35:06 +0700

    membuat message

--- src/BarcodeScanner.jsx
+++ src/BarcodeScanner.jsx
@@ -1,8 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { BrowserBarcodeReader } from "@zxing/library";
 
 const BarcodeScanner = () => {
     const videoRef = useRef(null);
+    const [resultMessage, setResultMessage] = useState("");
 
     const startScanner = () => {
         const codeReader = new BrowserBarcodeReader();
@@ -10,6 +11,7 @@ const BarcodeScanner = () => {
             .decodeFromInputVideoDevice(undefined, "video")
             .then((result) => {
                 console.log("Found barcode: ", result.text);
+                setResultMessage(result.text);
                 // Lakukan sesuatu dengan hasil pemindaian di sini
             })
             .catch((err) => console.error("Error: ", err));
@@ -19,6 +21,7 @@ const BarcodeScanner = () => {
         <div>
             <video ref={videoRef} id="video" width="300" height="200"></video>
             <button onClick={startScanner}>Start Scanner</button>
+            {resultMessage != "" && <h1>{resultMessage}</h1>}
         </div>
     );
 };
