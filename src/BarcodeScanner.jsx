1dce5a7de5ba0b0095fc89c01533dfabbdc45dab
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2024-04-14 17:04:27 +0700

    f

--- src/BarcodeScanner.jsx
+++ src/BarcodeScanner.jsx
@@ -1,35 +1,19 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import { BrowserBarcodeReader } from "@zxing/library";
 
 const BarcodeScanner = () => {
     const videoRef = useRef(null);
-    const codeReader = useRef(null);
     const [resultMessage, setResultMessage] = useState("");
 
-    useEffect(() => {
-        codeReader.current = new BrowserBarcodeReader();
-        startScanner();
-
-        return () => {
-            stopScanner();
-        };
-    }, []);
-
     const startScanner = () => {
-        if (videoRef.current) {
-            codeReader.current
-                .decodeFromInputVideoDevice(undefined, "video", (result) => {
-                    console.log("Found barcode: ", result.text);
-                    showAlert(result.text);
-                })
-                .catch((err) => console.error("Error: ", err));
-        }
-    };
-
-    const stopScanner = () => {
-        if (codeReader.current) {
-            codeReader.current.reset();
-        }
+        const codeReader = new BrowserBarcodeReader();
+        codeReader
+            .decodeFromInputVideoDevice(undefined, "video")
+            .then((result) => {
+                console.log("Found barcode: ", result.text);
+                showAlert(result.text);
+            })
+            .catch((err) => console.error("Error: ", err));
     };
 
     const showAlert = (text) => {
@@ -40,7 +24,7 @@ const BarcodeScanner = () => {
         <div>
             <video ref={videoRef} id="video" width="300" height="200"></video>
             <button onClick={startScanner}>Start Scanner</button>
-            {resultMessage !== "" && <h3>{resultMessage}</h3>}
+            {resultMessage != "" && <h3>{resultMessage}</h3>}
         </div>
     );
 };
